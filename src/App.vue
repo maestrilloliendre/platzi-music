7016d073189813d05e5891cef6893e461105a4a5
maestrilloliendre <profesor.liendres@gmail.com>
2017-11-09 20:54:43 +0100

    Captura de API REST, componentes Header y Footer

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,67 @@
 <template lang="pug">
   #app
-    img(src="./assets/logo.png")
-    h1 {{ msg }}
+    Pm-header
+    section.section
+      nav.nav.has-shadow
+        .container <!-- lo mismo que nav.container --> 
+          input.input.is-large(
+            type="text", 
+            placeholder="Buscar canciones", 
+            v-model="searchQuery"
+          )
+          a.button.is-info.is-large(@click="search") Buscar
+          a.button.is-danger.is-large &times;
+      .container      
+      p
+        small {{ searchMessage }}
+
+      .container
+        .columns
+          .column(v-for="track in tracks") 
+            | {{ track.name }} - {{ track.artists[0].name }}
+    pm-footer        
 </template>
 
 <script>
+import trackService from './services/track'
+import PmFooter from './components/layout/Footer.vue'
+import PmHeader from './components/layout/Header.vue'
+
 export default {
   name: 'app',
+
+  components: { PmFooter, PmHeader },
+
   data () {
     return {
-      msg: 'Hola Vue!'
+      searchQuery: '',
+      tracks: []
+    }
+  },
+
+  methods: {
+    search () {
+      if (!this.searchQuery) { return }
+      trackService.search(this.searchQuery)
+        .then(res => {
+          this.tracks = res.tracks.items
+        })
+    }
+  },
+
+  computed: {
+    searchMessage () {
+      return `Encontrados: ${this.tracks.length}`
     }
   }
 }
 </script>
 
 <style lang="scss">
-  @import './scss/main.scss'
+  @import './scss/main.scss';
+
+  .results {
+    margin-top: 50px
+  }
+
 </style>
---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,7 @@
+<template lang="pug">
+  footer.footer
+    .container
+        .content.has-text-centered
+            p @maestrilloliendre
+
+</template>
